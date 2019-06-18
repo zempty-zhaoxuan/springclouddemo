0f5e72ef527cb06f873e9791a8a8a390f89632ff
zempty <zempty.zhao@gosstech.com>
2019-06-18 16:41:50 +0800

    add zuul demo

---
+++ cloudzuuldemo3001/src/main/java/com/zempty/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.zempty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author zempty
+ * @create 2019-06-18 10:42 AM
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class);
+    }
+}
